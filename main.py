739045e59aadbc35f734758ca21e35f35f67e78a
dorinmoldovan <dorin.moldovan@yahoo.com>
2025-02-24 00:13:02 +0200

    updated the user interface

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from gui.gui import App
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec())
